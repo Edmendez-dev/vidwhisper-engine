8220e16b8bf36477bff491b2f33b791a95886705
Edmendez-dev <eduardodev00@gmail.com>
2026-01-13 22:19:16 -0600

    feat(cors): add CORS middleware to allow requests from localhost with Next.js. #2

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.core.database import connect_to_mongodb, close_mongodb_connection
 from app.core.minio import init_minio
@@ -19,6 +20,15 @@ app = FastAPI(
     description="API for VidWhisper backend services",
     version="1.0.0",
     lifespan=lifespan)
+
+# Add CORS middleware 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"], # Allow all HTTP Methods (GET, POST, UPDATE, DELETE, etc.)
+    allow_headers=["*"], # Allow all headers
+)
 
 app.include_router(router, prefix="/api/v1/transcriptions", tags=["transcriptions"])
 
