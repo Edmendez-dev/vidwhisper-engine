d3d771ed9e0ddd53f30503065a0711810f6c4adf
Edmendez-dev <eduardodev00@gmail.com>
2026-03-03 10:59:35 -0600

    feat(footer): integrate internationalization for footer component in English and Spanish. #5

--- frontend/components/vidwhisper/footer.tsx
+++ frontend/components/vidwhisper/footer.tsx
@@ -1,19 +1,22 @@
 "use-client";
 
+import { useTranslations } from "next-intl";
+
 // Function get year
 function getCurrentYear() {
   return new Date().getFullYear();
 }
 
 export default function Footer() {
+  const t = useTranslations("Footer");
+
   return (
     <>
       <footer className="flex items-center justify-center flex-col text-white/20 text-xs pb-4 gap-1">
         <div>
-          VidWhisper Engine · Transcripción con IA · &copy;{" "}
-          {getCurrentYear()}{" "}
+          {t("copyright")} &copy; {getCurrentYear()}{" "}
         </div>
-        <span className="text-violet-500/60">v1.0</span>
+        <span className="text-violet-500/60">{t("version")}</span>
       </footer>
     </>
   );
